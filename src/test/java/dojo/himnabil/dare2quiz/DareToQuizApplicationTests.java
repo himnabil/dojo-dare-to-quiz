ebe4970868f1df758a03aff5920ab8727faa84c4
himnabil <himnabil@gmail.com>
2018-04-09 21:24:10 +0200

    init

---
+++ src/test/java/dojo/himnabil/dare2quiz/DareToQuizApplicationTests.java
@@ -0,0 +1,16 @@
+package dojo.himnabil.dare2quiz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DareToQuizApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
